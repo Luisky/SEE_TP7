8a212aea245880d93dce3220ff7ab867be926900
Luis Thomas <luis.thomas2005@gmail.com>
2019-11-26 16:05:33 +0100

    initial commit

---
+++ export_K2000.sh
@@ -0,0 +1,21 @@
+
+#!/bin/sh
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+rm /run/media/luisky/rootfs/root/K2000_bbb
+cp K2000_bbb /run/media/luisky/rootfs/root/K2000_bbb
+umount /run/media/luisky/boot /run/media/luisky/rootfs
+
+if [ $? = 1 ]
+then
+        echo "error: is SD card plugged in ?"
+        exit 1
+fi
+
+echo "K2000_bbb updated on SD card, boot & rootfs partitions unmounted"
+
+exit 0
